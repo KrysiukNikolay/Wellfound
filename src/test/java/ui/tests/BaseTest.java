7d3119e527d8d2f61f3dbcd5907b56c1ca51033d
KrysiukNikolay <KrysiukNikolay@gmail.com>
2023-06-07 13:32:44 +0300

    refactor BaseTest

--- src/test/java/ui/tests/BaseTest.java
+++ src/test/java/ui/tests/BaseTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.BeforeEach;
 import ui.driver.DriverSetup;
 import ui.pages.HomePage;
 
-public class BaseTest extends HomePage {
-
+public class BaseTest {
+    HomePage homePage = new HomePage();
     @BeforeEach
     public void warmUP(){
-        openWebSite();
+        homePage.openWebSite();
 }
 
     @AfterEach
